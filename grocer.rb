9913c6aeeba4799cc1a4bcf0989e09e3ebb0192c
franco84 <franco84@gmail.com>
2016-01-12 00:47:38 +0000

    nitrous-backup at Tue Jan 12 00:47:38 UTC 2016

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,30 @@
 def consolidate_cart(cart:[])
   # code here
+  items = []
+  newcart = {}
+
+  cart.each do |x|
+    x.each do |item, data|
+      items << x.keys
+      if !newcart.has_key?(item)
+        newcart[item] = data
+      end
+    end
+  end
+  items.flatten!
+  counted = []
+  items.each do |x|
+    if !counted.include?(x)
+      count = items.count(x)
+      counted << x
+        newcart.each do |item, data|
+          if item == x
+            newcart[item][:count] = count
+          end
+        end
+    end
+  end
+  newcart
 end
 
 def apply_coupons(cart:[], coupons:[])
